6f8bf616eefdf05c8d5a78e75fd6d109fe770211
Oskar Sobczyk <sobczykoskar@gmail.com>
2017-03-09 00:39:01 +0100

    pracownia++

--- Algorytmy_i_struktury_danych/pracownia1/main1.cpp
+++ Algorytmy_i_struktury_danych/pracownia1/main1.cpp
@@ -14,11 +14,15 @@ int main()
     }
     int V = n * m;
     string wiersz;
-    vector<int> *sasiedztwa = new vector<int>[ (n * m) ];
+    //vector<int> *sasiedztwa = new vector<int>[ (n * m) + 1 ];
+    int sasiedztwa[V][4];
+    for(int i = 0;i<V;i++){
+        for(int j=0;j<4;j++) sasiedztwa[i][j]=0;
+    }
     int spojne[V];
     int licznik = 0;
 
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
         char buffer[m];
         if (scanf("%s", buffer) < 0)
@@ -29,10 +33,9 @@ int main()
 
     for (int i = 0; i < n * m; i++)
     {
-        if(wiersz[i] == 'A'){}
-        else if (wiersz[i] == 'B')
+        if (wiersz[i] == 'B')
         {
-            if ((wiersz[i + m] == 'C' || wiersz[i + m] == 'D' || wiersz[i + m] == 'F') && i / m != n - 1)
+            if ((wiersz[i + m] == 'C' || wiersz[i + m] == 'D' || wiersz[i + m] == 'F') && i / m != n + 1)
             {
                 sasiedztwa[i].push_back(i + m);
                 sasiedztwa[i + m].push_back(i);
@@ -42,7 +45,7 @@ int main()
         }
         else if (wiersz[i] == 'D')
         {
-            if ((wiersz[i + 1] == 'C' || wiersz[i + 1] == 'B' || wiersz[i + 1] == 'F') && (i % m) + 1 != m)
+            if ((wiersz[i + 1] == 'C' || wiersz[i + 1] == 'B' || wiersz[i + 1] == 'F') && i % m + 1 != m)
             {
                 sasiedztwa[i].push_back(i + 1);
                 sasiedztwa[i + 1].push_back(i);
@@ -52,12 +55,12 @@ int main()
         }
         else if (wiersz[i] == 'E')
         {
-            if ((wiersz[i + 1] == 'B' || wiersz[i + 1] == 'C' || wiersz[i + 1] == 'F') && (i % m) + 1 != m)
+            if ((wiersz[i + 1] == 'B' || wiersz[i + 1] == 'C' || wiersz[i + 1] == 'F') && i % m + 1 != m)
             {
                 sasiedztwa[i].push_back(i + 1);
                 sasiedztwa[i + 1].push_back(i);
             }
-            if ((wiersz[i + m] == 'C' || wiersz[i + m] == 'D' || wiersz[i + m] == 'F') && i / m != n - 1)
+            if ((wiersz[i + m] == 'C' || wiersz[i + m] == 'D' || wiersz[i + m] == 'F') && i / 4 != n + 1)
             {
                 sasiedztwa[i].push_back(i + m);
                 sasiedztwa[i + m].push_back(i);
@@ -67,22 +70,21 @@ int main()
         }
         else if (wiersz[i] == 'F')
         {
-            if ((wiersz[i + 1] == 'B' || wiersz[i + 1] == 'C' || wiersz[i + 1] == 'F') && (i % m) + 1 != m)
+            if ((wiersz[i + 1] == 'B' || wiersz[i + 1] == 'C' || wiersz[i + 1] == 'F') && i % m + 1 != m)
             {
                 //printf("test1");
                 sasiedztwa[i].push_back(i + 1);
                 sasiedztwa[i + 1].push_back(i);
             }
-            if ((wiersz[i + m] == 'C' || wiersz[i + m] == 'D' || wiersz[i + m] == 'F') && i / m != n - 1)
+            if ((wiersz[i + m] == 'C' || wiersz[i + m] == 'D' || wiersz[i + m] == 'F') && i / 4 != n + 1)
             {
+                //printf("test1");
 
                 sasiedztwa[i].push_back(i + m);
                 sasiedztwa[i + m].push_back(i);
             }
-            if (sasiedztwa[i].empty()){
-                //printf("test1");
+            if (sasiedztwa[i].empty())
                 sasiedztwa[i].push_back(-1);
-            }
         }
         else if (wiersz[i] == 'C')
         {
@@ -95,15 +97,15 @@ int main()
     {
         spojne[i] = 0;
     }
-
+    
     stack<int> stos;
 
     for (int i = 0; i < V; i++)
     {
         if (spojne[i] == 0 && sasiedztwa[i].empty() == false)
         {
+            //printf("%d\n",i);
             licznik += 1;
-            if(sasiedztwa[i][0]!=-1){
             stos.push(i);
             spojne[i] = licznik;
             while (stos.empty() == false)
@@ -114,11 +116,11 @@ int main()
                 {
                     if (spojne[sasiedztwa[v][j]] == 0)
                     {
+                        //printf("%d\n",sasiedztwa[v][j]);
                         stos.push(sasiedztwa[v][j]);
                         spojne[sasiedztwa[v][j]] = licznik;
                     }
                 }
-            }
             }
         }
     }
