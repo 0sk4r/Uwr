f3dec2b01d75b22742065b879427e7f5317aa248
Oskar <sobczykoskar@gmail.com>
2017-05-17 10:53:23 +0200

    cos dziala

--- Metody_Programowania/pracownia2/pracownia4/OskarSobczyk.hs
+++ Metody_Programowania/pracownia2/pracownia4/OskarSobczyk.hs
@@ -9,6 +9,7 @@ module OskarSobczyk (typecheck, eval) where
 -- Importujemy moduły z definicją języka oraz typami potrzebnymi w zadaniu
 import AST
 import DataTypes
+import qualified Data.Map as M
 
 -- Funkcja sprawdzająca typy
 -- Dla wywołania typecheck vars e zakładamy, że zmienne występujące
@@ -16,7 +17,7 @@ import DataTypes
 -- miało typ int
 -- UWAGA: to nie jest jeszcze rozwiązanie; należy zmienić jej definicję.
 typecheck :: [Var] -> Expr p -> TypeCheckResult p
-typecheck vars expr = Ok 
+typecheck vars expr = Ok
 
 -- Funkcja obliczająca wyrażenia
 -- Dla wywołania eval input e przyjmujemy, że dla każdej pary (x, v)
@@ -24,5 +25,56 @@ typecheck vars expr = Ok
 -- Możemy założyć, że wyrażenie e jest dobrze typowane, tzn.
 -- typecheck (map fst input) e = Ok
 -- UWAGA: to nie jest jeszcze rozwiązanie; należy zmienić jej definicję.
+
+--getVariable :: [(Var,Integer)] -> String -> Integer
+--getVariable zmienne var = M.lookup var M.fromList(zmienne)
+
 eval :: [(Var,Integer)] -> Expr p -> EvalResult
 eval kupa kupad = RuntimeError
+
+--eval zmienne (EIf p bexpr expr1 expr2) =
+--	if evalBExpr zmienne bexpr
+--		then evalExpr zmienne expr1
+--		else evalExpr zmienne expr2
+
+evalExpr :: [(Var,Integer)] -> Expr p -> Integer
+
+
+evalExpr zmienne (ENum p var) = var
+
+--evalExpr zmienne (EVar p var) = getVariable zmienne var
+
+evalExpr zmienne (EUnary p  UNeg expr) = (- evalExpr zmienne expr)
+
+evalExpr zmienne (EBinary p op expr1 expr2) =
+	case op of
+		BAdd -> evalExpr zmienne expr1 + evalExpr zmienne expr2
+		BMul -> evalExpr zmienne expr1 * evalExpr zmienne expr2
+		BSub -> evalExpr zmienne expr1 - evalExpr zmienne expr2
+		BDiv -> evalExpr zmienne expr1 `div` evalExpr zmienne expr2
+		BMod -> evalExpr zmienne expr1 `mod` evalExpr zmienne expr2
+
+evalExpr zmienne (EIf p bexpr expr1 expr2) =
+	if evalBExpr zmienne bexpr
+		then evalExpr zmienne expr1
+		else evalExpr zmienne expr2
+
+--evalExpr zmienne (ELet p var expr1 expr2) =
+--	evalExpr (zmienne ++ [(var,evalExpr zmienne expr1)]) expr2
+
+evalBExpr :: [(Var,Integer)] -> Expr p -> Bool
+
+evalBExpr zmienne (EBool p var) = var
+
+evalBExpr zmienne (EUnary p UNot expr) = not (evalBExpr zmienne expr)
+
+evalBExpr zmienne (EBinary p op expr1 expr2) =
+	case op of
+		BEq -> evalExpr zmienne expr1 == evalExpr zmienne expr2
+		BNeq -> evalExpr zmienne expr1 /= evalExpr zmienne expr2
+		BGe -> evalExpr zmienne expr1 >= evalExpr zmienne expr2
+		BGt -> evalExpr zmienne expr1 > evalExpr zmienne expr2
+		BLe -> evalExpr zmienne expr1 <= evalExpr zmienne expr2
+		BLt -> evalExpr zmienne expr1 < evalExpr zmienne expr2
+		BOr -> evalBExpr zmienne expr1 || evalBExpr zmienne expr2
+		BAnd -> evalBExpr zmienne expr1 && evalBExpr zmienne expr2
