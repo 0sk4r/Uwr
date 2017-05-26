b7a4a696ecd3a5c2a95222adcd15e5f2b0fdda48
Oskar <sobczykoskar@gmail.com>
2017-05-26 11:30:16 +0200

    prac5 init

---
+++ Metody_Programowania/pracownia2/pracownia5/OskarSobczykTests.hs
@@ -0,0 +1,18 @@
+-- Wymagamy, by moduł zawierał tylko bezpieczne funkcje
+{-# LANGUAGE Safe #-}
+-- Definiujemy moduł zawierający testy.
+-- Należy zmienić nazwę modułu na {Imie}{Nazwisko}Tests gdzie za {Imie}
+-- i {Nazwisko} należy podstawić odpowiednio swoje imię i nazwisko
+-- zaczynające się wielką literą oraz bez znaków diakrytycznych.
+module OskarSobczykTests(tests) where
+
+-- Importujemy moduł zawierający typy danych potrzebne w zadaniu
+import DataTypes
+
+-- Lista testów do zadania
+-- Należy uzupełnić jej definicję swoimi testami
+tests :: [Test]
+tests =
+  [ Test "inc"      (SrcString "input x in x + 1") (Eval [42] (Value 43))
+  , Test "undefVar" (SrcString "x")                TypeError
+  ]
