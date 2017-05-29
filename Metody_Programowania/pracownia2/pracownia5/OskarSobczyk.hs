c41a76c25614a4666edaa45c197cec4ba08e2e28
Oskar <sobczykoskar@gmail.com>
2017-05-29 15:49:36 +0200

    mp ...

--- Metody_Programowania/pracownia2/pracownia5/OskarSobczyk.hs
+++ Metody_Programowania/pracownia2/pracownia5/OskarSobczyk.hs
@@ -15,15 +15,21 @@ import DataTypes
 -- w vars są już zdefiniowane i mają typ int, i oczekujemy by wyrażenia e
 -- miało typ int
 -- UWAGA: to nie jest jeszcze rozwiązanie; należy zmienić jej definicję.
+
+type TypeEnv = (String,Type)
+
+
 typecheck :: [FunctionDef p] -> [Var] -> Expr p -> TypeCheckResult p
 typecheck _ _ _ = Ok
 
+
+checker :: [FunctionDef p] -> [TypeEnv] -> Expr p -> Either (Error p) Type
 
 checker functions types (EUnit p) = Right TUnit
 
 checker functions types (EPair p expr1 expr2) = 
 	case checker functions types expr1 of
-		Right type1 -> case checker types expr2 of
+		Right type1 -> case checker functions types expr2 of
 			Right type2 -> Right (TPair type1 type2)
 			Left err -> Left err
 		Left err -> Left err
@@ -47,6 +53,16 @@ checker functions types (ECons p expr1 expr2) =
 			Left err -> Left err
 			otherwise -> Left (ErrorType p "rozne typy w liscie")
 		Left err -> Left err
+
+checker functions types (EMatchL p expr1 nilclause (var1, var2, expr2)) = 
+	case checker functions types expr1 of
+		Right (TList type1) -> case checker functions types expr2 of
+			Right type2 -> case checker functions (types ++ [(var1, type1),(var2,(TList type2))]) expr2 of
+				Right type2 -> Right type2
+				Left err -> Left err
+				otherwise -> Left (ErrorType p "diff type in match")
+			Left err -> Left err
+		otherwise -> Left (ErrorType p "no list in match")
 
 
 -- Funkcja obliczająca wyrażenia
