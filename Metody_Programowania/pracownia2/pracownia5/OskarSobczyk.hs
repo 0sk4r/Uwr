4b8bd120d80cf90f48fb413490cebf3ef7932bdb
Oskar <sobczykoskar@gmail.com>
2017-06-01 14:40:17 +0200

    mp ..

--- Metody_Programowania/pracownia2/pracownia5/OskarSobczyk.hs
+++ Metody_Programowania/pracownia2/pracownia5/OskarSobczyk.hs
@@ -18,8 +18,38 @@ import qualified Data.Map  as Map
 -- UWAGA: to nie jest jeszcze rozwiązanie; należy zmienić jej definicję.
 
 type TypeEnv = (String,Type)
-data Error p = ErrorType p String deriving Show
+--data Error p = ErrorType p String deriving Show
 type FunctionEnv p = Map.Map FSym (FunctionDef p)
+
+type Error p = (p, ErrKind)
+data ErrKind 
+    = EUndefinedVariable Var 
+    | ETypeMismatch Type Type
+    | EIfMismatch Type Type 
+    | ETypeMismatchL Type Type 
+    | EMatchNoList Type
+
+
+instance Show ErrKind where
+  show (EUndefinedVariable x) =
+    "Undefined variable " ++ show x ++ "."
+  show (ETypeMismatch t1 t2)  =
+    "Type mismatch: expected " ++ show t1 ++ " but received " ++ show t2 ++ "."
+  show (EIfMismatch t1 t2)    =
+    "Type mismatch in the branches of an if: " ++ show t1 ++ " and " ++ show t2 ++ "."
+  show (ETypeMismatchL t1 t2)  =
+    "Type mismatch in list: head: " ++ show t1 ++ " tail: " ++ show t2 ++ "."
+  show (EMatchNoList t1)  =
+    "Expected list in match get" ++ show t1 ++ "."
+
+data EValue 
+    = Empty 
+    | MInt Integer 
+    | MBool Bool 
+    | MUnit 
+    | MPair (EValue, EValue) 
+    | MList [EValue]
+      deriving (Show, Eq, Ord)
 
 initList :: [Var] -> [TypeEnv]
 initList lst = [ (x,TInt) | x <- lst ]
@@ -34,17 +64,17 @@ typecheck functions vars expr =
     --initlist inicjuje liste [(var,TInt)]
     case checker fenv (initList vars) expr of
         Right TInt -> Ok
-        Left (ErrorType p err) -> Error p err
+        Left (p, err) -> Error p $ show err
         otherwise -> Error  (getData expr) "program not return int"
         where fenv = (Map.fromList $ zip (map funcName functions) functions)
 
-checkfunctions :: [FunctionDef p] -> FunctionEnv p -> Just (Errro p)
+--checkfunctions :: [FunctionDef p] -> FunctionEnv p -> Just (Errro p)
 
-checkfunctions fdefs fenv =
-    case checker fenv ([(funcArg def, funcArgType)]) (funcBody def) of
-        Left err -> Just err
-        Nothing -> Nothing
-        where def = head fdefs
+--checkfunctions fdefs fenv =
+--    case checker fenv ([(funcArg def, funcArgType)]) (funcBody def) of
+--        Left err -> Just err
+--        Nothing -> Nothing
+--       where def = head fdefs
 
 
 checker :: FunctionEnv p -> [TypeEnv] -> Expr p -> Either (Error p) Type
@@ -55,34 +85,36 @@ checker functions types (EBool p var) = Right TBool
 
 checker functions types (EVar p var) = case getVariable var types of
     Just x -> Right x
-    otherwise -> Left (ErrorType p ("undefined var: " ++ var))
+    otherwise -> Left (p, EUndefinedVariable var)
 
 checker functions types (EUnary p op expr) =
     case op of
         UNeg -> case checker functions types expr of 
             Right TInt -> Right TInt
-            otherwise -> Left (ErrorType p "-bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
+            Left err -> Left err
         UNot -> case checker functions types expr of 
             Right TBool -> Right TBool 
-            otherwise -> Left (ErrorType p "~int (expected bool)")
+            Right t -> Left (p, ETypeMismatch TBool t)
+            Left err ->Left err
 
 checker functions types (EBinary p BAdd expr1 expr2) = 
     case checker functions types expr1 of
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TInt
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... + bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) + ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 checker functions types (EBinary p BSub expr1 expr2) = 
     case checker functions types expr1 of
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TInt
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... - bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) - ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 checker functions types (EBinary p BMod expr1 expr2) = 
@@ -90,19 +122,18 @@ checker functions types (EBinary p BMod expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TInt
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... mod bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) mod ...")
-
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 checker functions types (EBinary p BMul expr1 expr2) = 
     case checker functions types expr1 of
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TInt
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... * bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) * ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 
@@ -111,10 +142,9 @@ checker functions types (EBinary p BDiv expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TInt
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... div bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) div ...")
-
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 checker functions types (EBinary p BGt expr1 expr2) = 
@@ -122,9 +152,9 @@ checker functions types (EBinary p BGt expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... > bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) < ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 checker functions types (EBinary p BGe expr1 expr2) = 
@@ -132,9 +162,9 @@ checker functions types (EBinary p BGe expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... => bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) <= ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 checker functions types (EBinary p BLt expr1 expr2) = 
@@ -142,9 +172,9 @@ checker functions types (EBinary p BLt expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... < bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) < ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 checker functions types (EBinary p BLe expr1 expr2) = 
@@ -152,9 +182,9 @@ checker functions types (EBinary p BLe expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... <= bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) <= ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 checker functions types (EBinary p BEq expr1 expr2) = 
@@ -162,18 +192,18 @@ checker functions types (EBinary p BEq expr1 expr2) =
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... = bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) = ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 checker functions types (EBinary p BNeq expr1 expr2) = 
     case checker functions types expr1 of
         Right TInt -> case checker functions types expr2 of
             Right TInt -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... <> bool (expected int)")
+            Right t -> Left (p, ETypeMismatch TInt t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "bool (expected int) <> ...")
+        Right t -> Left (p, ETypeMismatch TInt t)
 
 
 
@@ -182,33 +212,28 @@ checker functions types (EBinary p BAnd expr1 expr2) =
         Right TBool -> case checker functions types expr2 of
             Right TBool -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... and int (expected bool)")
+            Right t -> Left (p, ETypeMismatch TBool t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "int (expected bool) and ...")
+        Right t -> Left (p, ETypeMismatch TBool t)
 
 checker functions types (EBinary p BOr expr1 expr2) = 
     case checker functions types expr1 of
         Right TBool -> case checker functions types expr2 of
             Right TBool -> Right TBool
             Left err -> Left err
-            otherwise -> Left (ErrorType p "... or int (expected bool)")
+            Right t -> Left (p, ETypeMismatch TBool t)
         Left err -> Left err
-        otherwise -> Left (ErrorType p "int (expected bool) or ...")
+        Right t -> Left (p, ETypeMismatch TBool t)
 
 checker functions types (EIf p exbool expr1 expr2) =
     case checker functions types exbool of
         Right TBool -> case checker functions types expr1 of
-            Right TInt -> case checker functions types expr2 of
-                Right TInt -> Right TInt
+            Right t1 -> case checker functions types expr2 of
+                Right t2 -> if t1 == t2 then Right t1 else Left (p, EIfMismatch t1 t2)
                 Left err -> Left err
-                otherwise -> Left (ErrorType p "int and bool (expected int) in if")
             Left err -> Left err
-            Right TBool -> case checker functions types expr2 of
-                Right TBool -> Right TBool
-                Left err -> Left err
-                otherwise -> Left (ErrorType p "bool and int (expected bool) in if")
         Left err -> Left err
-        otherwise -> Left (ErrorType p "if (no bool expr) then ...")
+        Right t -> Left (p, ETypeMismatch TBool t)
 
 
 
@@ -250,10 +275,10 @@ checker functions types (ENil p typ) = Right (TList typ)
 
 checker functions types (ECons p expr1 expr2) = 
     case checker functions types expr1 of
-        Right type1 -> case checker functions types expr2 of
-            Right (TList type1) -> Right (TList type1)
+        Right t1 -> case checker functions types expr2 of
+            Right (TList t2) -> 
+                if t1 == t2 then Right (TList t1) else Left (p, ETypeMismatchL t1 t2)
             Left err -> Left err
-            otherwise -> Left (ErrorType p "rozne typy w liscie")
         Left err -> Left err
 
 checker functions types (EMatchL p expr1 nilclause (var1, var2, expr2)) = 
@@ -262,18 +287,18 @@ checker functions types (EMatchL p expr1 nilclause (var1, var2, expr2)) =
             Right type2 -> case checker functions (types ++ [(var1, type1),(var2,(TList type2))]) expr2 of
                 Right type2 -> Right type2
                 Left err -> Left err
-                oftherwise -> Left (ErrorType p ("diff type in match" ++ show type2))
+                Right t -> Left (p, ETypeMismatch type2 t)
             Left err -> Left err
-        otherwise -> Left (ErrorType p "no list in match")
+        Right t -> Left (p, EMatchNoList t)
 
 checker functions types (EApp p fsym expr) = 
     case findFnc functions fsym of
         Just definition -> case checker functions types expr of 
             Right x -> Right (funcResType definition) 
             Left err -> Left err
-            otherwise -> Left (ErrorType p ("arg dont match"))
+            Right t -> Left (p, ETypeMismatch  x t)
             where x = funcArgType definition
-        Nothing -> Left (ErrorType p "function undefined")
+        Nothing -> Left (p, ETypeMismatch TInt TInt)
         
 
 findFnc :: FunctionEnv p -> FSym -> Maybe (FunctionDef p)
@@ -287,3 +312,193 @@ findFnc functionEnv identifier = Map.lookup identifier functionEnv
 -- UWAGA: to nie jest jeszcze rozwiązanie; należy zmienić jej definicję.
 eval :: [FunctionDef p] -> [(Var,Integer)] -> Expr p -> EvalResult
 eval _ _ _ = RuntimeError
+
+
+
+evalExpr :: [FunctionEnv p] -> [(Var,EValue)] -> Expr p -> Either String EValue
+
+evalExpr fenv varenv (ENum p var) = Right (MInt var)
+
+evalExpr fenv varenv (EBool p var) =
+    case var of
+        True -> Right (MBool True)
+        False -> Right (MBool False)
+
+
+-- ???????????
+evalExpr fenv varenv (EVar p var) = case getVariable var varenv of
+    Just x -> Right x
+    otherwise -> Left "var undefined"
+
+-----------------------
+--Operatory unarne
+-----------------------
+
+
+evalExpr fenv varenv (EUnary p UNeg expr) =
+    case evalExpr fenv varenv expr of
+        Right (MInt val) -> Right (MInt (-val))
+        Left err -> Left err
+
+evalExpr fenv varenv (EUnary p UNot expr) =
+    case evalExpr fenv varenv expr of
+        Right (MBool val) -> Right (MBool (not val))
+        Left err -> Left err
+
+
+-----------------------
+--Operatory arytmetyczne
+-----------------------
+
+evalExpr fenv varenv (EBinary p BAdd expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MInt (val1 + val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BSub expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MInt (val1 - val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BMul expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MInt (val1 * val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BDiv expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt 0) -> Left "div by 0"
+            Right (MInt val2) -> Right (MInt (val1 `div` val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BMod expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt 0) -> Left "mod by 0"
+            Right (MInt val2) -> Right (MInt (val1 `mod` val2))
+            Left err -> Left err
+        Left err -> Left err
+
+
+-----------------------
+--Operatory logiczne
+-----------------------
+evalExpr fenv varenv (EBinary p BAnd expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MBool b1) -> case evalExpr fenv varenv expr2 of
+            Right (MBool b2) -> Right (MBool (b1 && b2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BOr expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MBool b1) -> case evalExpr fenv varenv expr2 of
+            Right (MBool b2) -> Right (MBool (b1 || b2))
+            Left err -> Left err
+        Left err -> Left err
+
+
+-----------------------
+--Operatory porownania
+-----------------------
+
+evalExpr fenv varenv (EBinary p BLt expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MBool (val1 < val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BLe expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MBool (val1 <= val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BGt expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MBool (val1 > val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BGe expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MBool (val1 >= val2))
+            Left err -> Left err
+        Left err -> Left err
+
+
+evalExpr fenv varenv (EBinary p BEq expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MBool (val1 == val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EBinary p BNeq expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right (MInt val1) -> case evalExpr fenv varenv expr2 of
+            Right (MInt val2) -> Right (MBool (val1 /= val2))
+            Left err -> Left err
+        Left err -> Left err
+
+-----------------------
+--If
+-----------------------
+
+evalExpr fenv varenv (EIf p exprbool expr1 expr2) =
+    case evalExpr fenv varenv exprbool of
+        Right (MBool True) -> case evalExpr fenv varenv expr1 of
+            Right val -> Right val
+            Left err -> Left err
+        Right (MBool False) -> case evalExpr fenv varenv expr2 of
+            Right val -> Right val
+            Left err -> Left err
+        Left err -> Left err
+
+-----------------------
+--Let
+-----------------------
+
+evalExpr fenv varenv (ELet p var expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        --Let w ktorym nadpisywana jest jakas wczesza zmienna bedzie dzialac poniewaz kiedy program dochodzi do EVar 
+        --najpierw przesukuje od konca tablicee wartosci boolowskich a dopieto potem int
+        Right val -> case evalExpr fenv (varenv ++ [(var,val)]) expr2 of
+            Right val2 -> Right val2
+            Left err -> Left err
+        --Jezeli zmienna ktora nadpisujemy ma miec wartosc int to mozemy usunac wystapienia tej zmiennej z tablicy bool poniewaz w przyslosci
+        --i tak nie wykorzystamy tych wartosci. Delete dziala w taki sposob ze jesli usuwanego elementu nie ma w tablicy to zwroci nie zmieniona tablice.
+        --Czyli mozemy bezpiecznie usuwac "nie istniejace" wpisy w tablicy bool
+        Left err -> Left err
+
+evalExpr fenv varenv (EPair p expr1 expr2) =
+    case evalExpr fenv varenv expr1 of
+        Right val1 -> case evalExpr fenv varenv expr2 of
+            Right val2 -> Right (MPair (val1,val2))
+            Left err -> Left err
+        Left err -> Left err
+
+evalExpr fenv varenv (EFst p expr) =
+    case evalExpr fenv varenv expr of
+        Right (MPair (val1,val2)) -> Right val1
+        Left err -> Left err
+
+
+evalExpr fenv varenv (ESnd p expr) =
+    case evalExpr fenv varenv expr of
+        Right (MPair (val1,val2)) -> Right val2
+        Left err -> Left err
+
+
