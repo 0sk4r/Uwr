8b6a6bc302d1926dde8793cb13d182dbc493f6e6
Sobczyk <sobczykoskar@gmail.com>
2017-10-18 22:28:55 +0200

    pythonr end

--- pythonr/lista02/zad02.py
+++ pythonr/lista02/zad02.py
@@ -1,6 +1,6 @@
 class Program:
-    def __init__(self, p1):
-        self.program = p1
+    def __init__(self, p):
+        self.program = p
 
     def exec(self, vars):
         try:
@@ -12,6 +12,30 @@ class Program:
 
     def __str__(self):
         return str(self.program)
+
+
+class Add(Program):
+    def __init__(self, e1, e2):
+        self.expr1 = e1
+        self.expr2 = e2
+
+    def exec(self, vars):
+        return self.expr1.exec(vars) + self.expr2.exec(vars)
+
+    def __str__(self):
+        return "%s + %s" % (self.expr1, self.expr2)
+
+
+class Minus(Program):
+    def __init__(self, e1, e2):
+        self.expr1 = e1
+        self.expr2 = e2
+
+    def exec(self, vars):
+        return self.expr1.exec(vars) - self.expr2.exec(vars)
+
+    def __str__(self):
+        return "%s - %s" % (self.expr1, self.expr2)
 
 
 class Mult(Program):
@@ -41,7 +65,7 @@ class Div(Program):
             return e1 * e2
 
     def __str__(self):
-        return "%s  %s" % (self.expr1, self.expr2)
+        return "%s / %s" % (self.expr1, self.expr2)
 
 
 class Var(Program):
@@ -83,6 +107,27 @@ class Assign(Program):
         return "%s = %s\n%s" % (self.variable, self.value, self.expr)
 
 
+class Bool(Program):
+    def __init__(self, e1, op, e2):
+        self.expresion1 = e1
+        self.expresion2 = e2
+        self.operator = op
+
+    def exec(self, vars):
+        v1 = self.expresion1.exec(vars)
+        v2 = self.expresion2.exec(vars)
+
+        usr_func = str(v1) + str(self.operator) + str(v2)
+        return eval(usr_func)
+
+    def __str__(self):
+        return "%s %s %s" % (
+            self.expresion1,
+            self.operator,
+            self.expresion2
+        )
+
+
 class Condition(Program):
     def __init__(self, x, e1, e2):
         self.bool = x
@@ -90,10 +135,11 @@ class Condition(Program):
         self.expr2 = e2
 
     def exec(self, vars):
-        if self.bool != 0:
-            return self.expr1.exec
+        lol = self.bool.exec(vars)
+        if lol != 0:
+            return self.expr1.exec(vars)
         else:
-            return self.expr2.exec
+            return self.expr2.exec(vars)
 
     def __str__(self):
         return "if %s:\n" \
@@ -103,13 +149,46 @@ class Condition(Program):
 
 
 class Loop(Program):
-    def __init__(self, var, end, expr):
-        self.
+    def __init__(self, var, start, stop, expr):
+        self.variable = var
+        self.startval = start
+        self.stopval = stop
+        self.expresion = expr
+
+    def exec(self, vars):
+        for i in range(self.startval, self.stopval):
+            vars[self.variable] = i
+            return self.expresion.exec(vars)
+
+    def __str__(self):
+        return "for %s in range(%s, %s):\n" \
+               "    %s" % (self.variable, self.startval, self.stopval, self.expresion)
 
 
 def main():
-    x = Program(Condition(Cons(1), Mult(Cons(1), Var("X")), Mult(Cons(1), Var("Y"))))
-    print(x)
+    add = Program(Add(Cons(2),Cons(3)))
+    print(add)
+    print(add.exec({}))
+
+    minus = Program(Minus(Cons(2), Cons(3)))
+    print(minus)
+    print(minus.exec({}))
+
+    mult = Program(Mult(Cons(2), Cons(3)))
+    print(mult)
+    print(add.exec({}))
+
+    div = Program(Div(Cons(2), Cons(0)))
+    print(div)
+    print(div.exec({}))
+    
+    x1 = Program(Condition(Bool(Cons(2), ">", Cons(3)), Mult(Cons(1), Var("X")), Mult(Cons(1), Var("Y"))))
+    print(x1.exec(({"X": 2, "Y": 4})))
+    print(x1)
+
+    x2 = Program(Loop("x", 1, 5, Mult(Cons(2),Var("x"))))
+    print(x2.exec({"X": 2, "Y": 4}))
+    print(x2)
 
 
 if __name__ == "__main__":
