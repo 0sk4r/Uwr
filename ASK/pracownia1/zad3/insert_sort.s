2da0ccccf58364c8bd9e8555ff04cc2cd91bf260
Oskar Sobczyk <oskar@MacBook-Pro-Oskar.local>
2018-04-23 23:09:13 +0200

    insert_sort done

--- ASK/pracownia1/zad3/insert_sort.s
+++ ASK/pracownia1/zad3/insert_sort.s
@@ -1,28 +1,47 @@
     .file   "insert_sort.s"
 
-    arg1 = %rdi
-    arg2 = %rsi
-    key1 = %r8
-    key2 = %r9
-    j = %r10
-    i = %r11
-    addres = %r12
+    ptr_s = %rdi #wskaznik na pierwszy element
+    ptr_e = %rsi #wskaznik na ostatni element
+    addres1 = %r8
+    addres2 = %r9
+    val1 = %r10
+    val2 = %r11
+    i = %r12
+    j = %r13
+    x = %r14
+
     .text
     .global insert_sort
     .type insert_sort, @function
 
 insert_sort:
     mov $1, i
-for:
-    lea 8(i, arg1), addres
-    mov (addres), key1
-    mov i, j
-    sub $1, j
-while:
-    lea 8(j, arg1), addres
-    mov (addres), key2
-    cmp key1, key2 #key2 - key1
-    jns switch
-    jmp for
 
+for:
+    lea (ptr_s,i,8), addres1
+    cmp addres1, ptr_e
+    js end
+    mov (addres1), val1
+    mov i, j
+    dec j
+while:
+    cmp $0, j #j>=0
+    js for2 
+
+    lea (ptr_s, j, 8), addres2 # *tab[j]
+    mov (addres2), val2 #tab[j]
+    cmp val1, val2 #tab[j] > temp = tab[i]
+    js for2
+    
+    lea 8(ptr_s,j,8), addres2
+    mov val2, (addres2) #tab[j+1] = tab[j]
+    dec j
+    jmp while
+for2:
+    lea 8(ptr_s,j,8), addres2
+    mov val1, (addres2) #tab[j+1] = temp
+    inc i #i++
+    jmp for
+end:
+    ret
     .size insert_sort, .-insert_sort
--- ASK/pracownia1/zad3/zad3.c
+++ ASK/pracownia1/zad3/zad3.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void insert_sort(long *first, long* last);
+extern void insert_sort(long *first, long *last);
 
-int main(){
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Za mało argumentów\n");
+        return -1;
+    }
+    long arr[100];
+    int n = argc - 1;
+    for (int i = 0; i < n; ++i)
+    {
+        arr[i] = atol(argv[1 + i]);
+    }
+    printf("nieposortowania:\n");
+
+    for (int i = 0; i < n; ++i)
+        printf("%ld\n", arr[i]);
+
+    insert_sort(arr, &arr[n - 1]);
+
+    printf("posortowana:\n");
+
+    for (int i = 0; i < n; ++i)
+        printf("%ld\n", arr[i]);
 
     return 0;
 }
